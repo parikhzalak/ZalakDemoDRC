35be07230c8a934d2bd939fe9bce7919fa74ff1e
Zalak Parikh <zalak.parikh@tatvasoft.com>
2021-09-07 19:26:33 +0530

    Initial commit with random UI design

---
+++ Zalak_DRCDemo/Zalak_DRCDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zalak_DRCDemo
+//
+//  Created by Zalak on 07/09/21.
+//  Copyright © 2021 Zalak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
